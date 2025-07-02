d2e2a785af74405e6e90ded423ce8bf1d506e7ad
jenn-jk <jenkins051@gmail.com>
2025-07-02 20:42:21 +0530

    items list added to home screen

--- lib/home.dart
+++ lib/home.dart
@@ -5,16 +5,51 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-Widget myBuilder(BuildContext context, BoxConstraints constraints) {
-  return Center(child: Text("Home Page"));
-}
-
 class _HomePageState extends State<HomePage> {
+  List<String> toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home Page")),
-      body: LayoutBuilder(builder: myBuilder),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Stack(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(20.0),
+                      height: constraints.maxHeight * 0.9,
+                      width: constraints.maxWidth * 0.9,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      child: ListView.builder(
+                        itemCount: toDoList.length,
+                        itemBuilder: (context, index) {
+                          return Text(toDoList[index]);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            toDoList.add("New Item Added ${toDoList.length}");
+          });
+        },
+        child: Icon(Icons.add_box_rounded),
+      ),
     );
   }
 }
